e4be421f9685b26cc8f50433a2626ba25cafb40c
Franco Borgiani <francoborgiani@outlook.com>
2021-06-20 12:00:47 -0300

    Primera versión del curso de JS Profesiional

---
+++ script.js
@@ -0,0 +1,26 @@
+const video = document.querySelector("video");
+const button = document.querySelector("button");
+
+function mediaPlayer(config){
+    this.media = config.el
+}
+mediaPlayer.prototype.play = function(){
+    this.media.play();
+};
+
+mediaPlayer.prototype.pause = function(){
+    this.media.pause()
+};
+mediaPlayer.prototype.togglePlay = function(){
+    if(this.media.paused) {
+        this.play();
+    }else{
+        this.pause();
+    }
+};
+
+const player = new mediaPlayer({el: video});
+
+button.onclick =()=> player.togglePlay();
+
+video.onclick =()=> player.togglePlay();
